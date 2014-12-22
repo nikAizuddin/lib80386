6a7430df93856488c4634dcd80c10efffc9349c3
NlCK <nickaizuddin93@gmail.com>
2014-12-22 21:34:05 +0800

    Added examples and improve revision history

---
+++ src/io/append_string/example/001_gettingStarted/_start.asm
@@ -0,0 +1,79 @@
+;          1         2         3         4         5         6         7
+;01234567890123456789012345678901234567890123456789012345678901234567890
+;=======================================================================
+;+---------------------------------------------------------------------+
+;|     EXAMPLE 001: Getting Started                                    |
+;+---------------------------------------------------------------------+
+;|          AUTHOR: Nik Mohamad Aizuddin bin Nik Azmi                  |
+;|    DATE CREATED: 22-DEC-2014                                        |
+;| EXAMPLE PURPOSE: Demonstrates how to use the function               |
+;|                  append_string().                                   |
+;+---------------------------------------------------------------------+
+;|        LANGUAGE: x86 Assembly Language                              |
+;|          SYNTAX: Intel                                              |
+;|       ASSEMBLER: NASM                                               |
+;|    ARCHITECTURE: i386                                               |
+;|          KERNEL: Linux 32-bit                                       |
+;|          FORMAT: elf32                                              |
+;|  EXTERNAL FILES: append_string.asm                                  |
+;+---------------------------------------------------------------------+
+;=======================================================================
+
+extern append_string
+
+section .data
+    string1: db "Hello World", "           "
+    strlen1: dd 11
+
+    string2: db "! I am good"
+    strlen2: dd 11
+
+section .text
+global _start
+_start:
+
+;+---------------------------+
+;| Append string2 to string1 |==========================================
+;+---------------------------+
+;|     Lets say,
+;|         string1 = "Hello World"
+;|         string2 = "! I am good"
+;|     after appending the string2 to the string1, the string1 will
+;|     becomes "Hello World! I am good"
+;=======================================================================
+    ;    +---------------------+
+    ;----| Call append_string; |----------------------------------------
+    ;    +---------------------+
+    ;       append_string( @string1, @strlen1, @string2, strlen2 );
+    ;-------------------------------------------------------------------
+    sub    esp, 16                  ;reserve 16 bytes
+    mov    eax, string1             ;get @string1
+    mov    ebx, strlen1             ;get @strlen1
+    mov    ecx, string2             ;get @string2
+    mov    edx, [strlen2]           ;get strlen2
+    mov    [esp     ], eax          ;arg1: @string1
+    mov    [esp +  4], ebx          ;arg2: @strlen1
+    mov    [esp +  8], ecx          ;arg3: @string2
+    mov    [esp + 12], edx          ;arg4: strlen2
+    call   append_string
+    add    esp, 16                  ;restore 16 bytes
+
+;+-----------------------------+
+;| Print the string1 to stdout |========================================
+;+-----------------------------+
+;=======================================================================
+    ;    +------------------+
+    ;----| Systemcall write |-------------------------------------------
+    ;    +------------------+
+    ;       write( stdout, @string1, strlen1 );
+    ;-------------------------------------------------------------------
+    mov    eax, 0x04                ;systemcall write
+    mov    ebx, 0x01                ;write to stdout
+    mov    ecx, string1             ;src = @string1
+    mov    edx, [strlen1]           ;strlen = strlen1
+    int    0x80
+
+.exit:
+    mov    eax, 0x01                ;systemcall exit
+    xor    ebx, ebx                 ;return 0
+    int    0x80
