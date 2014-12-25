a717ce282f758510ad5f16238832954ac741096d
NlCK <nickaizuddin93@gmail.com>
2014-12-26 05:51:09 +0800

    Documentations update

--- src/io/print_double2string/example/001_gettingStarted/_start.asm
+++ src/io/print_double2string/example/001_gettingStarted/_start.asm
@@ -5,9 +5,9 @@
 ;|     EXAMPLE 001: Getting Started                                    |
 ;+---------------------------------------------------------------------+
 ;|          AUTHOR: Nik Mohamad Aizuddin bin Nik Azmi                  |
-;|    DATE CREATED: 22-DEC-2014                                        |
+;|    DATE CREATED: 26-DEC-2014                                        |
 ;| EXAMPLE PURPOSE: Demonstrates how to use the function               |
-;|                  append_string.                                     |
+;|                  print_double2string.                               |
 ;+---------------------------------------------------------------------+
 ;|        LANGUAGE: x86 Assembly Language                              |
 ;|          SYNTAX: Intel                                              |
@@ -15,21 +15,22 @@
 ;|    ARCHITECTURE: i386                                               |
 ;|          KERNEL: Linux 32-bit                                       |
 ;|          FORMAT: elf32                                              |
-;|  EXTERNAL FILES: append_string.asm                                  |
+;|  EXTERNAL FILES: print_double2string.asm                            |
 ;+---------------------------------------------------------------------+
 ;=======================================================================
 
-extern append_string
+extern print_double2string
 global _start
 
-section .data
+section .bss
 
-    string1: db "Hello World",\
-                "           " ;blank spaces. Reserved for string2.
-    strlen1: dd 11
+    out_string: resd 6
+    out_strlen: resd 1
 
-    string2: db "! I am good"
-    strlen2: dd 11
+section .rdata
+
+    double_value:   dq -2147483647.1234567
+    decimal_places: dd 10000000
 
 section .text
 
@@ -41,37 +42,37 @@ _start:
 ;///////////////////////////////////////////////////////////////////////
 
 
-;+-------------------------------------------------------+
-;| Append string2 to string1 and print string1 to stdout |==============
-;+-------------------------------------------------------+
+;+---------------------------------------------------------+
+;| Convert double_value to ASCII value and print to stdout |============
+;+---------------------------------------------------------+
 ;=======================================================================
 
-    ;    +---------------------------+
-    ;----| append string2 to string1 |---------------------------------
-    ;    +---------------------------+
-    ;       append_string( @string1, @strlen1, @string2, strlen2 );
+    ;    +-------------------------------+
+    ;----| convert double_value to ASCII |------------------------------
+    ;    +-------------------------------+
     ;-------------------------------------------------------------------
-    sub    esp, 16                  ;reserve 16 bytes
-    mov    eax, string1             ;get @string1
-    mov    ebx, strlen1             ;get @strlen1
-    mov    ecx, string2             ;get @string2
-    mov    edx, [strlen2]           ;get strlen2
-    mov    [esp     ], eax          ;arg1: @string1
-    mov    [esp +  4], ebx          ;arg2: @strlen1
-    mov    [esp +  8], ecx          ;arg3: @string2
-    mov    [esp + 12], edx          ;arg4: strlen2
-    call   append_string
-    add    esp, 16                  ;restore 16 bytes
+    sub    esp, 20                  ;reserve 20 bytes
+    mov    eax, [double_value]      ;get double_value[0]
+    mov    ebx, [double_value + 4]  ;get double_value[1]
+    mov    ecx, [decimal_places]    ;get decimal_places
+    mov    edx, out_string          ;get @out_string     
+    mov    [esp     ], eax          ;arg1: double_value[0]
+    mov    [esp +  4], ebx          ;arg1: double_value[1]
+    mov    [esp +  8], ecx          ;arg2: decimal_places
+    mov    [esp + 12], edx          ;arg3: @out_string
+    mov    eax, out_strlen          ;get @out_strlen
+    mov    [esp + 16], eax          ;arg4: @out_strlen
+    call   print_double2string
+    add    esp, 20                  ;restore 20 bytes
 
-    ;    +------------------------------------+
-    ;----| systemcall write string1 to stdout |-------------------------
-    ;    +------------------------------------+
-    ;       write( stdout, @string1, strlen1 );
+    ;    +---------------------------------------+
+    ;----| systemcall write out_string to stdout |----------------------
+    ;    +---------------------------------------+
     ;-------------------------------------------------------------------
     mov    eax, 0x04                ;systemcall write
-    mov    ebx, 0x01                ;write to stdout
-    mov    ecx, string1             ;src = @string1
-    mov    edx, [strlen1]           ;strlen = strlen1
+    mov    ebx, 0x01                ;to stdout
+    mov    ecx, out_string          ;src string
+    mov    edx, [out_strlen]        ;strlen
     int    0x80
 
 
@@ -84,3 +85,4 @@ _start:
     mov    eax, 0x01                ;systemcall exit
     xor    ebx, ebx                 ;return 0
     int    0x80
+
